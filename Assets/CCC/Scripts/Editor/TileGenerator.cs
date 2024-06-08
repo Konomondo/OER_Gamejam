28532a330b089ebf6af9ed1cfa400825a450a77f
Nick Blust <75218083+NickBlust@users.noreply.github.com>
2024-06-08 20:09:15 +0200

    Rename tiles

--- Assets/CCC/Scripts/Editor/TileGenerator.cs
+++ Assets/CCC/Scripts/Editor/TileGenerator.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using CCC.Scripts.Editor;
 using UnityEditor;
-using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public static class TileGenerator
 {
@@ -14,14 +10,7 @@ public static class TileGenerator
         var names = tileInfo.GetColumn(1, 2);
         for (int i = 0; i < 23; i++)
         {
-            var tile = AssetDatabase.LoadAssetAtPath<TileBase>($"Assets/CCC/Tiles/Tiles_{i}.asset");
-            // AssetDatabase.RenameAsset($"Assets/CCC/Tiles/Tiles_{i}.asset");
-            Debug.Log(names[i] + " => " + tile.name);
-        }
-
-        foreach (var name in names)
-        {
-            Debug.Log(name);
+            AssetDatabase.RenameAsset($"Assets/CCC/Tiles/Tiles_{i}.asset", names[i]);
         }
     }
 }
